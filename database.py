dba1136e18e7f8b809a87dfbe32703dff856ba0a
Andrea Orru <andreaorru1991@gmail.com>
2016-10-01 17:32:35 -0400

    More refactoring.

--- database.py
+++ database.py
@@ -10,6 +10,16 @@ class ReferenceType(IntEnum):
     INDIRECT = 1
 
 
+def unpack(result, function=None):
+    def default_function(row):
+        return row[0]
+
+    if function is None:
+        function = default_function
+
+    return map(function, result.fetchall())
+
+
 class Database:
     def __init__(self, database_path):
         self.connection = sqlite3.connect(database_path)
@@ -21,28 +31,29 @@ class Database:
             instructions = self.c.execute('SELECT * FROM instructions WHERE pc >= ? AND pc <= ?', (start, end))
         else:
             instructions = self.c.execute('SELECT * FROM instructions')
-        return map(Instruction.from_row, instructions.fetchall())
+        return unpack(instructions, Instruction.from_row)
 
     def references(self, address, typ=None):
         if typ is None:
-            references = self.c.execute('SELECT * FROM references_ WHERE pointer=?', (address,))
+            references = self.c.execute('SELECT pointee FROM references_ WHERE pointer=?', (address,))
         else:
-            references = self.c.execute('SELECT * FROM references_ WHERE pointer=? AND type=?', (address, typ))
-        return map(lambda x: x['pointee'], references.fetchall())
+            references = self.c.execute('SELECT pointee FROM references_ WHERE pointer=? AND type=?', (address, typ))
+        return unpack(references)
 
     def referenced_by(self, address, typ=None):
         if typ is None:
-            referenced_by = self.c.execute('SELECT * FROM references_ WHERE pointee=?', (address,))
+            referenced_by = self.c.execute('SELECT pointer FROM references_ WHERE pointee=?', (address,))
         else:
-            referenced_by = self.c.execute('SELECT * FROM references_ WHERE pointee=? AND type=?', (address, typ))
-        return map(lambda x: x['pointer'], referenced_by.fetchall())
+            referenced_by = self.c.execute('SELECT pointer FROM references_ WHERE pointee=? AND type=?', (address, typ))
+        return unpack(referenced_by)
 
     def subroutines(self):
         subroutines = self.c.execute("""
-            SELECT *
+            SELECT pointee
             FROM instructions,
                  references_
             WHERE instructions.pc = references_.pointer
               AND instructions.opcode IN {}
         """.format(OpcodeCategory.CALL))
-        return map(lambda x: x['pointee'], subroutines.fetchall())
+
+        return unpack(subroutines)
