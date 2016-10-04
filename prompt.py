03a3d630b32325933a609a43c2dc2a92eccf0e8b
Andrea Orru <andreaorru1991@gmail.com>
2016-10-03 21:42:07 -0400

    Allow redirecting to a file.

--- prompt.py
+++ prompt.py
@@ -1,3 +1,5 @@
+from contextlib import redirect_stdout
+
 from database import LabelType
 from instruction import ReferenceType
 
@@ -17,35 +19,46 @@ class Prompt:
             print('>>> ', end="")
 
             line = input().split(' ')
-            self.command = line[0]
-            self.parameters = line[1:]
+            command = line[0]
 
-            if self.command == '':
-                continue
-            elif self.command in ('d', 'disassembly'):
-                self._disassembly()
-            elif self.command in ('dr', 'direct_references'):
-                self._references(ReferenceType.DIRECT)
-            elif self.command in ('ir', 'indirect_references'):
-                self._references(ReferenceType.INDIRECT)
-            elif self.command in ('r', 'references'):
-                self._references()
-            elif self.command in ('drb', 'directly_referenced_by'):
-                self._referenced_by(ReferenceType.DIRECT)
-            elif self.command in ('irb', 'indirectly_referenced_by'):
-                self._referenced_by(ReferenceType.INDIRECT)
-            elif self.command in ('rb', 'referenced_by'):
-                self._referenced_by()
-            elif self.command in ('l', 'labels'):
-                self._labels()
-            elif self.command in ('s', 'subroutines'):
-                self._labels(LabelType.SUBROUTINE)
-            elif self.command in ('v', 'vectors'):
-                self._labels(LabelType.VECTOR)
-            elif self.command in ('e', 'q', 'exit', 'quit'):
-                return
+            if line[-2: -1] == ['>']:
+                redirect_file = line[-1]
+                self.parameters = line[:-2]
+
+                with open(redirect_file, 'w') as f:
+                    with redirect_stdout(f):
+                        self._dispatch_command(command)
             else:
-                print('ERROR: unknown command "{}"'.format(self.command))
+                self.parameters = line[1:]
+                self._dispatch_command(command)
+
+    def _dispatch_command(self, command):
+        if command == '':
+            return
+        elif command in ('d', 'disassembly'):
+            self._disassembly()
+        elif command in ('dr', 'direct_references'):
+            self._references(ReferenceType.DIRECT)
+        elif command in ('ir', 'indirect_references'):
+            self._references(ReferenceType.INDIRECT)
+        elif command in ('r', 'references'):
+            self._references()
+        elif command in ('drb', 'directly_referenced_by'):
+            self._referenced_by(ReferenceType.DIRECT)
+        elif command in ('irb', 'indirectly_referenced_by'):
+            self._referenced_by(ReferenceType.INDIRECT)
+        elif command in ('rb', 'referenced_by'):
+            self._referenced_by()
+        elif command in ('l', 'labels'):
+            self._labels()
+        elif command in ('s', 'subroutines'):
+            self._labels(LabelType.SUBROUTINE)
+        elif command in ('v', 'vectors'):
+            self._labels(LabelType.VECTOR)
+        elif command in ('e', 'q', 'exit', 'quit'):
+            exit()
+        else:
+            print('ERROR: unknown command "{}"'.format(command))
 
     def _disassembly(self):
         for instruction in self.database.instructions(*map(unhex, self.parameters)):
