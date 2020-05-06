1e4f92ba2bc7ec348c80c1c5ff44653a9aaa352e
Andrea Orru <andrea@orru.io>
2020-05-06 14:36:47 +0900

    CPU comments

--- gilgamesh/cpu.py
+++ gilgamesh/cpu.py
@@ -12,21 +12,34 @@ class CPU:
     def __init__(self, log, pc: int, p: int, subroutine: int):
         self.log = log
         self.rom = log.rom
+
+        # Processor state.
         self.pc = pc
         self.state = State(p)
-        self.state_assertion = StateChange()
+
+        # Change in CPU state caused by the execution of the current subroutine.
         self.state_change = StateChange()
+        # What we know about the CPU state based on the
+        # sequence of instructions we have executed.
+        self.state_assertion = StateChange()
+
+        # Stack formed as a result of sequences of PHP/PLP instructions.
         self.state_stack: List[Tuple[State, StateChange]] = []
+        # The subroutine currently being executed.
         self.subroutine = subroutine
 
     @property
     def instruction_id(self) -> InstructionID:
+        # Get the ID of the instruction currently being executed
+        # in the context of the current subroutine.
         return InstructionID(self.pc, self.state.p, self.subroutine)
 
     def copy(self, new_subroutine=False) -> "CPU":
+        # Copy the current state of the CPU.
         cpu = copy(self)
         cpu.state = copy(self.state)
         cpu.state_assertion = copy(self.state_assertion)
+        # Don't carry over the state change information to new subroutines.
         cpu.state_change = StateChange() if new_subroutine else copy(self.state_change)
         return cpu
 
@@ -36,8 +49,10 @@ class CPU:
             keep_going = self.step()
 
     def step(self) -> bool:
+        # We can't analyze code that lives in RAM.
         if self.is_ram(self.pc):
             return False
+        # Don't visit the exact same instruction twice.
         if self.log.is_visited(self.instruction_id):
             return False
 
@@ -52,11 +67,13 @@ class CPU:
     def execute(self, instruction: Instruction) -> bool:
         self.pc += instruction.size
 
+        # See if we can learn something about the *required*
+        # state of the CPU based on the current instruction.
         self._derive_state_assertion(instruction)
 
         if instruction.is_return:
             self.log.add_subroutine_state(self.subroutine, self.state_change)
-            return False
+            return False  # Terminate the execution of this subroutine.
         elif instruction.is_interrupt:
             return self._unknown_subroutine_state()
         elif instruction.is_call:
@@ -72,35 +89,47 @@ class CPU:
         elif instruction.operation == Op.PLP:
             self.pop_state()
 
-        return True
+        return True  # Keep executing in the context of this subroutine.
 
     def branch(self, instruction: Instruction) -> None:
+        # Run a parallel instance of the CPU to follow
+        # the case in which we don't take the branch.
         cpu = self.copy()
         cpu.run()
 
         target = instruction.absolute_argument
         assert target is not None
 
+        # Log the fact that the current instruction references the
+        # instruction pointed by the branch. Then take the branch.
         self.log.add_reference(instruction, target)
         self.pc = target
 
     def call(self, instruction: Instruction) -> bool:
         target = instruction.absolute_argument
         if target is None:
+            # If we can't reliably derive the address of the subroutine
+            # being called, we're left in an unknown state.
             return self._unknown_subroutine_state()
 
         self.log.add_reference(instruction, target)
         self.log.add_subroutine(target)
 
+        # Run a parallel instance of the CPU to execute
+        # the subroutine that is being called.
         cpu = self.copy(new_subroutine=True)
         cpu.subroutine = target
         cpu.pc = target
         cpu.run()
 
+        # If we univocally know what the return state of the
+        # called subroutine is, we can propagate it to the
+        # current CPU state. Otherwise, to be on the safe
+        # side, we need to stop the execution.
         known = self._propagate_subroutine_state(target)
         if not known:
             return self._unknown_subroutine_state()
-        return known
+        return True
 
     def jump(self, instruction: Instruction) -> bool:
         target = instruction.absolute_argument
@@ -121,6 +150,11 @@ class CPU:
             self.state.reset(arg)
             self.state_change.reset(arg)
 
+        # Simplify the state change by applying our knowledge
+        # of the current state. I.e. if we know that the
+        # processor is operating in 8-bits accumulator mode
+        # and we switch to that same mode, effectively no
+        # state change is being performed.
         self.state_change.apply_assertion(self.state_assertion)
 
     def push_state(self) -> None:
@@ -134,6 +168,8 @@ class CPU:
         return (address <= 0x001FFF) or (0x7E0000 <= address <= 0x7FFFFF)
 
     def _derive_state_assertion(self, instruction: Instruction) -> None:
+        # If we have defined a custom assertion for the
+        # current instruction, retrieve it and apply it.
         saved_assertion = self.log.state_assertions.get(instruction.pc)
         if saved_assertion:
             if saved_assertion.m is not None:
@@ -142,6 +178,10 @@ class CPU:
                 self.state_assertion.x = saved_assertion.x
             return
 
+        # If we're executing an instruction with a certain operand size,
+        # and no state change has been performed in the current subroutine,
+        # then we can infer that the state of the processor as we enter
+        # the subroutine *must* be the same in all cases.
         if (
             instruction.address_mode == AddressMode.IMMEDIATE_M
             and self.state_change.m is None
@@ -154,6 +194,8 @@ class CPU:
             self.state_assertion.x = self.state.x
 
     def _propagate_subroutine_state(self, subroutine: int) -> bool:
+        # If the subroutine can return in more than one distinct state,
+        # we can't reliably propagate the state to the caller.
         state_changes = self.log.get_subroutine_states(subroutine)
         if len(state_changes) != 1:
             return False
