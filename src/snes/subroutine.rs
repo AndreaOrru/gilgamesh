25f2b17d06b6ef0939df5162a95446ed634d9a0c
Andrea Orru <andrea@orru.io>
2020-06-17 10:32:56 +0900

    subroutine.rs

--- src/snes/subroutine.rs
+++ src/snes/subroutine.rs
@@ -3,7 +3,7 @@ use std::collections::{BTreeSet, HashSet};
 use getset::{CopyGetters, Getters};
 
 use crate::snes::instruction::Instruction;
-use crate::snes::state::SubStateChange;
+use crate::snes::state::{SubStateChange, UnknownReason};
 
 /// Structure representing a subroutine.
 #[derive(Debug, CopyGetters, Getters)]
@@ -20,8 +20,8 @@ pub struct Subroutine {
     #[getset(get = "pub")]
     state_changes: HashSet<SubStateChange>,
 
-    #[getset(get_copy = "pub")]
-    has_unknown_state_change: bool,
+    #[getset(get = "pub")]
+    unknown_state_changes: HashSet<SubStateChange>,
 }
 
 impl Subroutine {
@@ -32,7 +32,7 @@ impl Subroutine {
             label,
             instructions: BTreeSet::new(),
             state_changes: HashSet::new(),
-            has_unknown_state_change: false,
+            unknown_state_changes: HashSet::new(),
         }
     }
 
@@ -43,16 +43,30 @@ impl Subroutine {
 
     /// Add a state change to the subroutine.
     pub fn add_state_change(&mut self, state_change: SubStateChange) {
-        self.state_changes.insert(state_change);
         if state_change.unknown() {
-            self.has_unknown_state_change = true;
+            self.unknown_state_changes.insert(state_change);
+        } else {
+            self.state_changes.insert(state_change);
         }
+    }
+
+    /// Return true if the subroutine has an unknown state change, false otherwise.
+    pub fn has_unknown_state_change(&self) -> bool {
+        !self.unknown_state_changes.is_empty()
+    }
+
+    /// Return true if the subroutine is unknown because of `reason`, false otherwise.
+    pub fn is_unknown_because_of(&self, reason: UnknownReason) -> bool {
+        self.unknown_state_changes
+            .iter()
+            .any(|s| s.unknown_reason() == reason)
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::snes::state::UnknownReason;
 
     #[test]
     fn test_add_state_change() {
@@ -61,7 +75,7 @@ mod tests {
         subroutine.add_state_change(SubStateChange::new_empty());
         assert!(!subroutine.has_unknown_state_change());
 
-        subroutine.add_state_change(SubStateChange::new_unknown());
+        subroutine.add_state_change(SubStateChange::new_unknown(UnknownReason::Unknown));
         assert!(subroutine.has_unknown_state_change());
     }
 }
