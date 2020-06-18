fd5302eb657f8e2a40e43035bfff7c3c4f5dce55
Andrea Orru <andrea@orru.io>
2020-06-18 12:51:55 +0900

    Registers tests

--- src/snes/register.rs
+++ src/snes/register.rs
@@ -1,5 +1,6 @@
 use crate::snes::state::StateRegister;
 
+/// 8/16-bit SNES register.
 #[derive(Copy, Clone)]
 pub struct Register {
     is_accumulator: bool,
@@ -8,6 +9,7 @@ pub struct Register {
 }
 
 impl Register {
+    /// Instantiate a Register object.
     pub fn new(is_accumulator: bool) -> Self {
         Self {
             is_accumulator,
@@ -16,6 +18,7 @@ impl Register {
         }
     }
 
+    /// Return the size of the register in the given state.
     pub fn size(&self, state: StateRegister) -> usize {
         if self.is_accumulator {
             state.a_size()
@@ -24,6 +27,7 @@ impl Register {
         }
     }
 
+    /// Get the value of the register in the given state.
     pub fn get(&self, state: StateRegister) -> Option<u16> {
         match self.size(state) {
             1 => self.lo,
@@ -31,6 +35,7 @@ impl Register {
         }
     }
 
+    /// Get the 16-bit value of the register.
     pub fn get_whole(&self) -> Option<u16> {
         if self.lo.is_none() || self.hi.is_none() {
             None
@@ -40,6 +45,7 @@ impl Register {
         }
     }
 
+    /// Set the value of the register in the given state.
     pub fn set(&mut self, state: StateRegister, value: Option<u16>) {
         match value {
             Some(v) => {
@@ -58,6 +64,7 @@ impl Register {
         }
     }
 
+    /// Set the 16-bit value of the register.
     pub fn set_whole(&mut self, value: Option<u16>) {
         match value {
             Some(v) => {
@@ -69,5 +76,67 @@ impl Register {
                 self.hi = None;
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_size() {
+        let mut state = StateRegister::from_mx(true, true);
+        let a = Register::new(true);
+        let x = Register::new(false);
+
+        assert_eq!(a.size(state), 1);
+        assert_eq!(x.size(state), 1);
+
+        state.set_m(false);
+        assert_eq!(a.size(state), 2);
+
+        state.set_x(false);
+        assert_eq!(x.size(state), 2);
+    }
+
+    #[test]
+    fn test_get_set() {
+        let mut state = StateRegister::from_mx(true, true);
+        let mut a = Register::new(true);
+
+        // Only lower 8-bits known.
+        a.set(state, Some(0xFF));
+        assert_eq!(a.get(state).unwrap(), 0xFF);
+        // 16-bits unknown.
+        state.set_m(false);
+        assert!(a.get(state).is_none());
+
+        // 16-bits known.
+        a.set(state, Some(0xFFFF));
+        assert_eq!(a.get(state).unwrap(), 0xFFFF);
+        // 8-bits known.
+        state.set_m(true);
+        assert_eq!(a.get(state).unwrap(), 0xFF);
+
+        // 16-bits unknown.
+        state.set_m(false);
+        a.set(state, None);
+        assert!(a.get(state).is_none());
+        // 8-bits unknown.
+        state.set_m(true);
+        assert!(a.get(state).is_none());
+    }
+
+    #[test]
+    fn test_get_set_whole() {
+        let mut state = StateRegister::from_mx(true, true);
+        let mut a = Register::new(true);
+
+        a.set_whole(Some(0xFFFF));
+        assert_eq!(a.get(state).unwrap(), 0xFF);
+        assert_eq!(a.get_whole().unwrap(), 0xFFFF);
+
+        state.set_m(false);
+        assert_eq!(a.get(state).unwrap(), 0xFFFF);
     }
 }
