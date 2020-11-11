2ac07bc155dd4e42e383ea0c242de77e2f3519cb
Andrea Orru <andrea@orru.io>
2020-11-12 00:13:18 +0900

    Editor

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,5 +1,18 @@
 #include "mainwindow.h"
 
 MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent) {
+  setupEditor();
+
+  setCentralWidget(editor);
   setWindowTitle(tr("Gilgamesh"));
+}
+
+void MainWindow::setupEditor() {
+  QFont font;
+  font.setFamily("monospace");
+  font.setFixedPitch(true);
+  font.setPointSize(12);
+
+  editor = new QTextEdit;
+  editor->setFont(font);
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -1,10 +1,16 @@
 #pragma once
 
 #include <QMainWindow>
+#include <QTextEdit>
 
 class MainWindow : public QMainWindow {
   Q_OBJECT
 
  public:
   MainWindow(QWidget* parent = nullptr);
+
+ private:
+  void setupEditor();
+
+  QTextEdit* editor;
 };
