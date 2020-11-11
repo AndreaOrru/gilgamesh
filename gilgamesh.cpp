ebaa5da1c5d1c1adda2fc694f31ab29747638b36
Andrea Orru <andrea@orru.io>
2020-11-11 22:30:16 +0900

    Basic QT application

---
+++ gilgamesh.cpp
@@ -0,0 +1,14 @@
+#include <QApplication>
+#include <QWidget>
+
+int main(int argc, char *argv[]) {
+  QApplication app(argc, argv);
+
+  QWidget window;
+
+  window.resize(250, 150);
+  window.setWindowTitle("Simple example");
+  window.show();
+
+  return app.exec();
+}
