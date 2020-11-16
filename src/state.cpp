9a1372e8d9e673ef87d5048a3bf00ea6ecd7f4cd
Andrea Orru <andrea@orru.io>
2020-11-16 23:08:02 +0900

    State comments

--- src/state.cpp
+++ src/state.cpp
@@ -2,51 +2,75 @@
 
 using namespace std;
 
-State::State(u8 p) : p{p} {}
+/***********
+ *  State  *
+ ***********/
 
+// Constructors.
+State::State() : State(true, true) {}
+State::State(u8 p) : p{p} {}
 State::State(bool m, bool x) : x{x}, m{m} {}
 
-State::State() : State(true, true) {}
-
+// Size of A in bytes.
 std::size_t State::sizeA() const {
   return m ? 1 : 2;
 }
+
+// Size of X in bytes.
 std::size_t State::sizeX() const {
   return x ? 1 : 2;
 }
 
+// Set bits in P.
 void State::set(u8 mask) {
   p |= mask;
 }
 
+// Reset bits in P.
 void State::reset(u8 mask) {
   p &= !mask;
 }
 
+// Comparison function.
 bool State::operator==(const State& other) const {
   return p == other.p;
 }
 
+/*****************
+ *  StateChange  *
+ *****************/
+
+// New empty state change (no changes).
 StateChange::StateChange() : StateChange(UnknownReason::Known) {}
 
+// New unknown state change.
 StateChange::StateChange(UnknownReason unknownReason)
     : unknownReason{unknownReason} {}
 
+// New state change.
 StateChange::StateChange(optional<bool> m, optional<bool> x)
     : m{m}, x{x}, unknownReason{UnknownReason::Known} {}
 
+// Set bits that were changed to 1 in P.
 void StateChange::set(u8 mask) {
   auto change = State(mask);
   m = change.m ? true : m;
   x = change.x ? true : x;
 }
 
+// Reset bits that were changed to 1 in P.
 void StateChange::reset(u8 mask) {
   auto change = State(mask);
   m = change.m ? false : m;
   x = change.x ? false : x;
 }
 
+// Return whether the state is unknown.
+bool StateChange::unknown() {
+  return unknownReason != UnknownReason::Known;
+}
+
+// Simplify the state change based on a state inference.
 void StateChange::applyInference(StateChange inference) {
   if (m.has_value() && (m == inference.m)) {
     m = nullopt;
@@ -56,14 +80,10 @@ void StateChange::applyInference(StateChange inference) {
   }
 }
 
-bool StateChange::unknown() {
-  return unknownReason != UnknownReason::Known;
-}
-
+// Hash table utils.
 bool StateChange::operator==(const StateChange& other) const {
   return m == other.m && x == other.x;
 };
-
 std::size_t hash_value(const StateChange& stateChange) {
   size_t seed = 0;
   boost::hash_combine(seed, stateChange.m);
--- src/state.hpp
+++ src/state.hpp
@@ -6,34 +6,42 @@
 
 #include "types.hpp"
 
+/**
+ * SNES state register (P).
+ */
 struct State {
   union {
     struct {
-      bool c : 1;
-      bool z : 1;
-      bool i : 1;
-      bool d : 1;
-      bool x : 1;
-      bool m : 1;
-      bool v : 1;
-      bool n : 1;
+      bool c : 1;  // Carry flag.
+      bool z : 1;  // Zero flag.
+      bool i : 1;  // Interrupt flag.
+      bool d : 1;  // Decimal flag.
+      bool x : 1;  // Index size flag.
+      bool m : 1;  // Accumulator size flag.
+      bool v : 1;  // Overflow flag.
+      bool n : 1;  // Negative flag.
     };
-    u8 p;
+    u8 p;  // State register.
   };
 
+  // Constructors.
   State();
   State(u8 p);
   State(bool m, bool x);
 
-  std::size_t sizeA() const;
-  std::size_t sizeX() const;
+  std::size_t sizeA() const;  // Size of A in bytes.
+  std::size_t sizeX() const;  // Size of X in bytes.
 
-  void set(u8 mask);
-  void reset(u8 mask);
+  void set(u8 mask);    // Set bits in P.
+  void reset(u8 mask);  // Reset bits in P.
 
+  // Comparison function.
   bool operator==(const State& other) const;
 };
 
+/**
+ * Possible reasons why a state change is unknown.
+ */
 enum class UnknownReason {
   Known,
   Unknown,
@@ -45,23 +53,32 @@ enum class UnknownReason {
   MutableCode,
 };
 
+/**
+ * State change caused by the execution of a subroutine.
+ */
 struct StateChange {
+  // New empty state change (no changes).
   StateChange();
+  // New unknown state change.
   StateChange(UnknownReason unknownReason);
+  // New state change.
   StateChange(std::optional<bool> m, std::optional<bool> x);
 
-  void set(u8 mask);
-  void reset(u8 mask);
+  void set(u8 mask);    // Set bits that were changed to 1 in P.
+  void reset(u8 mask);  // Reset bits that were changed to 1 in P.
+  bool unknown();       // Return whether the state is unknown.
+  // Simplify the state change based on a state inference.
   void applyInference(StateChange inference);
-  bool unknown();
 
+  // Hash table utils.
   bool operator==(const StateChange& other) const;
   friend std::size_t hash_value(const StateChange& stateChange);
 
-  std::optional<bool> m;
-  std::optional<bool> x;
-  UnknownReason unknownReason;
+  std::optional<bool> m;        // Accumulator size flag.
+  std::optional<bool> x;        // Index size flag.
+  UnknownReason unknownReason;  // Reason for state being unknown, if any.
 };
 
+// Set of StateChanges.
 typedef std::unordered_set<StateChange, boost::hash<StateChange>>
     StateChangeSet;
